5349a095e37ed051e039bf25fc95e2168ac0f9c2
Omar Estrella <omar@dynaboard.com>
2023-10-27 12:24:44 -0400

    different model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer
 from pydantic import BaseModel
+import torch
 
 app = FastAPI()
 
@@ -10,9 +11,25 @@ class Texts(BaseModel):
 
 
 @app.post("/jina-embeddings")
-async def embeddings(texts: Texts):
+async def jina_mbeddings(texts: Texts):
     model = AutoModel.from_pretrained(
         "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True
     )
     embeddings = model.encode(texts.texts)
+    return dict(embeddings=embeddings.tolist())
+
+
+@app.post("/llm-embedder")
+async def llm_embeddings(texts: Texts):
+    tokenizer = AutoTokenizer.from_pretrained("BAAI/llm-embedder")
+    model = AutoModel.from_pretrained("BAAI/llm-embedder", trust_remote_code=True)
+    model.eval()
+
+    encoded_input = tokenizer(
+        texts.texts, padding=True, truncation=True, return_tensors="pt"
+    )
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+        embeddings = model_output[0][:, 0]
+    embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
     return dict(embeddings=embeddings.tolist())
